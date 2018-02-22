dd90059134c6dccb04770c9dbbb6ea2234e03e65
copperware <jawad.jahangir.666@gmail.com>
2018-02-22 08:54:34 +0600

    header is now aware of user authentication state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (<li><a href="/auth/google">Login With Google</a></li>);
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
     render() {
         return (
             <nav>
                 <div className='nav-wrapper'>
-                    <a className='left brand-logo'>Email Gun</a>
+                    <Link to={this.props.auth ? '/surveys' : '/'} 
+                        className='left brand-logo'>Email Gun</Link>
                     <ul className='right'>
-                        <li><a>Login with Google</a></li>
+                        {renderContent}
                     </ul>
                 </div>
             </nav>
@@ -15,4 +28,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+let mapStateToProps = function(state) {
+    return { auth: state.auth };
+};
+
+export default connect(mapStateToProps, null)(Header);
