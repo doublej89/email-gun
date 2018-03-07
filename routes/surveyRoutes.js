518b2a0c6b565d4ffa3aefe9208e5219dfffc744
copperware <jawad.jahangir.666@gmail.com>
2018-03-08 04:24:29 +0600

    feedback collection system done!

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,10 +4,41 @@ const requireCredits = require('../middlewares/requireCredits');
 const Survey = mongoose.model('surveys');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const lodash = require('lodash');
+const Path = require('path-parser');
+const {URL} = require('url');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
+    });
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        const events = lodash.chain(req.body)
+            .map(({email, url}) => {        
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return {email, surveyId: match.surveyId, choice: match.choice};
+                } 
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({surveyId, email, choice}) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: {email: email, responded: false}
+                    }
+                }, {
+                    $set: {'recipients.$.responded': true},
+                    $inc: {[choice]: 1},
+                    dateResponded: new Date()                     
+                }).exec();
+            })
+            .value();
+            
+            res.send({});
     });
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
