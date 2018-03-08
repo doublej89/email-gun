b8a3fff1107e9ce75ca1ba1184eea3ce1bbdde92
copperware <jawad.jahangir.666@gmail.com>
2018-03-09 04:29:34 +0600

    users can now see their survey list

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,6 +9,11 @@ const Path = require('path-parser');
 const {URL} = require('url');
 
 module.exports = app => {
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey.find({_user: req.user.id}).select({recipients: false});
+        res.send(surveys);
+    });
+
     app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
     });
